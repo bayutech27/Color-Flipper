8166c2013ab8eeca17a2d72f2eddec74dc435b38
Bayo Alabi <bayutech27@gmail.com>
2025-11-11 23:54:45 +0100

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,7 +1,7 @@
 
 // Step 1: create a const variable for the hex
 
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 7, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 
 // Step 2: target button and color names
